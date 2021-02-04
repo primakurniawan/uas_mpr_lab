881551344348c00767c8590e4eb917a7fd62c9cd
lonelykatana <erickgultom325@gmail.com>
2021-02-04 23:06:50 +0700

    Menambahkan listview

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,33 +30,33 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
     Button logout;
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        InputStream inputStream = getResources().openRawResource(R.raw.foodlist);
+        CSVFile csvFile = new CSVFile(inputStream);
+        List<String[]> foodList = csvFile.read();
+        MyListAdapter adapter=new MyListAdapter(this, R.layout.listrow,R.id.txtid, foodList);
+        ListView listView=(ListView)findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
         logout = findViewById(R.id.logout);
-
-
         readDatabase();
-
-
-
-
-
-
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,20 +65,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, StartActivity.class));
             }
         });
-
-
     }
-
     private void readDatabase() {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("text");
-
     }
+    private class CSVFile {
+        InputStream inputStream;
 
-
-
-
-
-
+        public CSVFile(InputStream inputStream){
+            this.inputStream = inputStream;
+        }
+        public List<String[]> read(){
+            //
+            List<String[]> resultList = new ArrayList<String[]>();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            try {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] row = line.split(",");
+                    resultList.add(row);
+                }
+            }
+            catch (IOException e) {
+                Log.e("Main",e.getMessage());
+            }
+            finally {
+                try {
+                    inputStream.close();
+                }
+                catch (IOException e) {
+                    Log.e("Main",e.getMessage());
+                }
+            }
+            return resultList;
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/example/firebase/MyListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.firebase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyListAdapter extends ArrayAdapter<String[]> {
+    int groupid;
+    List<String[]> items;
+    Context context;
+    String path;
+
+    public MyListAdapter(Context context, int vg, int id, List<String[]> items){
+        super(context,vg, id, items);
+        this.context=context;
+        groupid=vg;
+        this.items=items;
+
+    }
+    static class ViewHolder {
+        public TextView textid;
+        public TextView textname;
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View rowView = convertView;
+        if(rowView==null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView= inflater.inflate(groupid, parent, false);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.textid = (TextView) rowView.findViewById(R.id.txtid);
+            viewHolder.textname = (TextView) rowView.findViewById(R.id.txtname);
+            rowView.setTag(viewHolder);
+        }
+        // Fill data
+        ViewHolder holder = (ViewHolder) rowView.getTag();
+        String[] row=items.get(position);
+        holder.textid.setText(row[0]);
+        holder.textname.setText(row[1]);
+
+        return rowView;
+    }
+
+}
+
